677acedddbf8a3ad32864c75f2da730e760d6bc3
SohamChk <career.soham.dev@gmail.com>
2024-02-23 09:40:40 +0530

    resolve redis config for custom `host` and `port`

--- dns-resolver/config/redis.js
+++ dns-resolver/config/redis.js
@@ -2,8 +2,10 @@ import { Config } from './config.js';
 import { createClient } from 'redis';
 
 const redisClient = createClient({
+  socket:{
     host: Config.REDIS_HOST,
     port: Config.REDIS_PORT,
+  }
   });
 
 export { redisClient }
